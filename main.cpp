d8d2f234205fcee19d3f549a848ff84a54d09425
orlandovaldez <orlando.valdez@my.tccd.edu>
2020-04-23 19:41:17 +0000

    Fixed Increment and Decrement to Class Style

--- main.cpp
+++ main.cpp
@@ -7,16 +7,6 @@
 #include <fstream> // write to a file
 using namespace std; 
 
-//***********Struct Style ************//
-struct MenuItem
-{
-  string name;
-  double itemCost; 
-  string desc; 
-  char addLetter; 
-  char removeLetter;
-  int count; 
-};
 
 //**********Class Style ************//
 class MenuItemList
@@ -44,45 +34,7 @@ class MenuItemList
     //void print() {} // print menu item data on demand}
 
 };
-  //**********Class Style ************//
-//function definitions
-void populateMenu(vector<MenuItem> &entireMenu)
-{
-  //put some default values in our Menu
-  const int numItems = 7; 
-  MenuItem Item1; 
-  MenuItem Item2;
-  MenuItem Item3; 
-  MenuItem Item4;
-  MenuItem Item5;
-  MenuItem Item6;
-  MenuItem Item7;    
 
-  entireMenu.push_back(Item1); //add to the end of list the Item1
-  entireMenu.push_back(Item2); //add to the end of list the Item2
-  entireMenu.push_back(Item3); //add to the end of list the Item3
-  entireMenu.push_back(Item4); //add to the end of list the Item4
-  entireMenu.push_back(Item5); //add to the end of list the Item5
-  entireMenu.push_back(Item6); //add to the end of list the Item6
-  entireMenu.push_back(Item7); //add to the end of list the Item7
-
-  vector<string> defaultMenuNames = {"Green Tea", "Burrito", "Item 3", "Item 4", "Item 5", "Item 6", "Item 7"}; 
-  vector<char> defaultAddLetters = {'A', 'B', 'C', 'D', 'E', 'F', 'G'}; 
-  vector<char> defaultRemoveLetters = {'a', 'b', 'c', 'd', 'e', 'f', 'g'}; 
-
-  for(int i = 0; i < numItems; i++)
-  {
-    //add each item to the default list efficiently 
-    entireMenu[i].name = defaultMenuNames[i]; 
-    entireMenu[i].addLetter = defaultAddLetters[i]; 
-    entireMenu[i].removeLetter = defaultRemoveLetters[i]; 
-    entireMenu[i].itemCost = (3.00 + i); //set a random starter cost for each item
-    entireMenu[i].count = 0; //initialze all counts to 0
-    entireMenu[i].desc = "delicious"; //set all default desc to "delicous"
-  }
-
-
-}
 
 //************Class Style*****/
 //function definitions
@@ -124,21 +76,6 @@ void populateObjectMenu(vector<MenuItemList> &entireMenu)
 }
 
 
-//********Struct Style*************/
-void showMenu(vector<MenuItem> &m)
-{
-  cout << fixed << setprecision(2);//set doubles to 2 decimal places
-  cout << "DrT's Effcient Menu" << endl; 
-  cout << "ADD  \tNAME \t COST \tREMOVE\tCOUNT\tDESC"<<endl; 
-  for(int i = 0; i < m.size(); i++)
-  {
-    cout << m[i].addLetter << ")" << setw(10) << m[i].name 
-    << setw(5) << "$" << m[i].itemCost << setw(5) << "(" << m[i].removeLetter
-    << ")" << setw(7) << m[i].count << setw(13) << m[i].desc 
-    <<endl; 
-  }
-
-}
 
 //**********Classs Style**********/
 void showobjectMenu(vector<MenuItemList> &m)
@@ -156,70 +93,19 @@ void showobjectMenu(vector<MenuItemList> &m)
 
 }
 
-void acceptOrder(vector<MenuItem> &m)
-{
-  char option = '\0';// the user-selected menu item
-  double subtotal = 0.0; 
 
-  do
-  {
-    cout << "\nPlease choose an item (x to Exit): ";
-    cin >> option; 
-
-    for(int i=0; i < m.size(); i++)
-    {
-      if(option == m[i].addLetter)
-      {
-        cout << "\nMenu Item " << m[i].addLetter << " selected."; 
-        m[i].count++; //increment the count by 1
-        cout << "\033[2J\033[1;1H"; //clear screen 
-        cout << "\n1 UP on " << m[i].name << endl;
-        subtotal += m[i].itemCost; //increment the subtotal by the cost of the item 
-        showMenu(m); //show the updated menu
-        cout << "\nSubtotal: $" << subtotal << endl;  
-      }
-      else if(option == m[i].removeLetter)
-      {
-        cout << "\nRemove Item " << m[i].removeLetter << " selected."; 
-        if(m[i].count > 0) //subtract if and only if the count is > 0
-        {
-          m[i].count--; //decrement the count by 1
-          cout << "\033[2J\033[1;1H"; //clear screen 
-          cout << "\n1 DOWN on " << m[i].name << endl;
-          subtotal -= m[i].itemCost; //decrement the subtotal by the cost of the item
-          showMenu(m); //show the updated menu
-          cout << "\nSubtotal: $" << subtotal << endl;  
-        }
-        else //the the user why you blocked item removal 
-        {
-            
-            cout << "\nItem count must be > 0 to remove: " << m[i].name << endl;
-        }
-      }
-      else if(
-                option != m[i].addLetter && 
-                option != m[i].removeLetter &&
-                option != 'x' &&
-                option != 'X' 
-            ) //test for all of my valid inputs
-            {
-              if(i == 0)
-              {
-                cout << "\nInvalid input (" << option << "). Please try again." << endl; 
-              }  
-            }
-    }
-  }while(option != 'x' && option != 'X'); 
-  cout << "\nThank you for placing your order." << endl; 
-  //handle the tip process here
-  //handle reciept generation here
-}
 
 //********Class Style**********
 void acceptobjectOrder(vector<MenuItemList> &m)
 {
   char option = '\0';// the user-selected menu item
   double subtotal = 0.0; 
+
+  double tipRate = 0.20;
+  double taxRate = 0.0825;
+  double tip = 0.0;
+  double tax = 0.0;
+  
 
   do
   {
@@ -231,7 +117,7 @@ void acceptobjectOrder(vector<MenuItemList> &m)
       if(option == m[i].getAddLetter())
       {
         cout << "\nMenu Item " << m[i].getAddLetter() << " selected."; 
-        m[i].setCount(++i); //increment the count by 1
+        m[i].setCount(m[i].getCount()+1) ;//increment the count by 1
         cout << "\033[2J\033[1;1H"; //clear screen 
         cout << "\n1 UP on " << m[i].getName() << endl;
         subtotal += m[i].getItemCost(); //increment the subtotal by the cost of the item 
@@ -243,7 +129,7 @@ void acceptobjectOrder(vector<MenuItemList> &m)
         cout << "\nRemove Item " << m[i].getRemoveLetter() << " selected."; 
         if(m[i].getCount() > 0) //subtract if and only if the count is > 0
         {
-          m[i].setCount(--i); //decrement the count by 1
+          m[i].setCount(m[i].getCount()-1); //decrement the count by 1
           cout << "\033[2J\033[1;1H"; //clear screen 
           cout << "\n1 DOWN on " << m[i].getName() << endl;
           subtotal -= m[i].getItemCost(); //decrement the subtotal by the cost of the item
@@ -270,6 +156,15 @@ void acceptobjectOrder(vector<MenuItemList> &m)
             }
     }
   }while(option != 'x' && option != 'X'); 
+
+  ///**cout << "Would You like To Add A Tip? (20% is the default)" << endl;;
+  cin >> tipRate;
+  tipRate = tipRate * (0.10);
+  tip = subtotal * tipRate ;
+  cout << tip << endl;
+  subtotal = (subtotal + tip);
+  cout << subtotal ;
+  //**/
   cout << "\nThank you for placing your order." << endl; 
   //handle the tip process here
   //callculate total due + tax + tip//accept payment type
@@ -284,7 +179,10 @@ void printTextReciept(vector<MenuItemList> &m)
   fstream reciept; 
   reciept.open("reciept.txt",ios::out);
 
-  reciept << m[0].getName() << endl; 
+  reciept << "\t" <<"Item Name" << "\t\t\t" << "Quantity " << endl;
+  reciept << "\t" << m[0].getName() << "\t\t" << m[0].getCount() << endl; 
+  
+ 
 
   reciept.close();  
 
@@ -304,17 +202,12 @@ void printTextReciept(vector<MenuItemList> &m)
 int main() 
 {
   
-//*******Struct Style********/
-  vector<MenuItem> wholeMenu; 
-  populateMenu(wholeMenu); //put some default values in the menu
-  showMenu(wholeMenu); //print the current data of the menu on screen 
-  acceptOrder(wholeMenu); 
-
 
 //*******Class Style********/
 vector<MenuItemList> objectMenu; 
 populateObjectMenu(objectMenu); //put some default values in the menu
 showobjectMenu(objectMenu); //show the inital menu on screen
+acceptobjectOrder(objectMenu);
 //solve and call acceptObjectOrder void function here
 
 printTextReciept(objectMenu); 
